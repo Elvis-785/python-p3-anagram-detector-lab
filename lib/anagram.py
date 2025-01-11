b7bae12f2b000d8c17e2752bae37d4f78982d78c
Elvis-785 <kiruielvis3358@gmail.com>
2025-01-11 11:54:04 +0300

    created anagram with oop

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,15 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = sorted(word.lower())
+
+    def match(self, possible_anagrams):
+        matches = []
+        for possible_anagram in possible_anagrams:
+            if sorted(possible_anagram.lower()) == self.word:
+                matches.append(possible_anagram)
+        return matches
+    
+listen = Anagram("listen")
+print(listen.match(['enlists', 'google', 'inlets', 'banana']))
